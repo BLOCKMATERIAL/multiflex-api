39d2265e5d7832e0f951afd2542baae4212014b5
Myroslav Hryhschenko <46076319+BLOCKMATERIAL@users.noreply.github.com>
2020-03-17 20:13:47 +0200

    Route for VIEWS

--- routes/films.js
+++ routes/films.js
@@ -8,6 +8,21 @@ var fs = require('fs');
 var mysql = require('../database.js');
 
 
+
+router.get('/chat', function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('./views/index.html', null, function (error, data) {
+        if (error) {
+            res.writeHead(404);
+            res.write('File not found!');
+        } else {
+            res.write(data);
+        }
+        res.end();
+    });
+
+});
+
 router.get('/films/:id' , (req, res) => {
     mysql.query("SELECT film_id, poster_url, title_name, genre_name, release_date, film_review, trailer_url FROM films WHERE film_id = ? ",[req.params.id], (err, rows, fields) => {
         if (!err)
